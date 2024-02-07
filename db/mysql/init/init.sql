99bd07b9c1ec98bf9a8f0b8c4972f686e3b6caae
yunut <somewheres@nate.com>
2024-02-07 22:45:11 +0900

    add: manager schema

--- db/mysql/init/init.sql
+++ db/mysql/init/init.sql
@@ -1,1 +1,8 @@
 USE caffe;
+
+CREATE TABLE manager (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `phone_number` VARCHAR(13) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
