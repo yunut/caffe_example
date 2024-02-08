956865fabf4e9c9b8aeaff1b0194d84f6c9b8fe9
yunut <somewheres@nate.com>
2024-02-08 15:28:53 +0900

    change: member table add unique

--- db/mysql/init/init.sql
+++ db/mysql/init/init.sql
@@ -4,5 +4,6 @@ CREATE TABLE user (
     `id` INT NOT NULL AUTO_INCREMENT,
     `phone_number` VARCHAR(13) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE (phone_number)
 );
