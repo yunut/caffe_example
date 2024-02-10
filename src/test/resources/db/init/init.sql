3ebff21e037116dd12a023bfe715aa611e8ed0c6
yunut <somewheres@nate.com>
2024-02-10 20:30:45 +0900

    add: add product table

--- src/test/resources/db/init/init.sql
+++ src/test/resources/db/init/init.sql
@@ -12,19 +12,16 @@ CREATE TABLE user (
 );
 
 CREATE TABLE product (
-    `id` VARCHAR(36) NOT NULL,
+    `bar_code` VARCHAR(36) NOT NULL,
     `category` VARCHAR(255) NOT NULL,
     `sale_price` INT NOT NULL,
     `origin_price` INT NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `description` TEXT NOT NULL,
-    `bar_code` VARCHAR(255) NOT NULL,
     `expire_date` DATE NOT NULL,
     `size` VARCHAR(20) NOT NULL,
     `created_by` VARCHAR(36) NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    UNIQUE (bar_code)
-
+    PRIMARY KEY (bar_code)
 )
