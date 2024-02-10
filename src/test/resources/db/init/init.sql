6e70cf6ea0ee75ced30d0fbd9317b83f9d4ac41f
yunut <somewheres@nate.com>
2024-02-10 19:43:02 +0900

    change: user id type change int to uuid

--- src/test/resources/db/init/init.sql
+++ src/test/resources/db/init/init.sql
@@ -1,7 +1,7 @@
 USE caffe;
 
 CREATE TABLE user (
-    `id` INT NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(36) NOT NULL,
     `phone_number` VARCHAR(13) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `roles` VARCHAR(255) NOT NULL,
@@ -10,3 +10,21 @@ CREATE TABLE user (
     PRIMARY KEY (id),
     UNIQUE (phone_number)
 );
+
+CREATE TABLE product (
+    `id` VARCHAR(36) NOT NULL,
+    `category` VARCHAR(255) NOT NULL,
+    `sale_price` INT NOT NULL,
+    `origin_price` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `bar_code` VARCHAR(255) NOT NULL,
+    `expire_date` DATE NOT NULL,
+    `size` VARCHAR(20) NOT NULL,
+    `created_by` VARCHAR(36) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    UNIQUE (bar_code)
+
+)
