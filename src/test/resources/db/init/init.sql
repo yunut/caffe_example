580d48165215b66de2c1ce6492cd548208bdd432
yunut <somewheres@nate.com>
2024-02-11 18:49:06 +0900

    change: init db rollback

--- src/test/resources/db/init/init.sql
+++ src/test/resources/db/init/init.sql
@@ -24,10 +24,5 @@ CREATE TABLE product (
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (bar_code),
-    UNIQUE (name, created_by),
-    INDEX (sale_price, bar_code),
-    INDEX (origin_price, bar_code),
-    INDEX (expire_date, bar_code),
-    INDEX (created_at, bar_code),
-    INDEX (updated_at, bar_code)
+    UNIQUE (name, created_by)
 )
